d90cca0e2382053ce23ac6afd83fd3c0d7c4c130
Michele <me@unkn0wn.cc>
2026-02-21 12:02:02 +0800

    clean(MoveDisabler): 清除狗屎代码

--- MoveDisabler.java
+++ MoveDisabler.java
@@ -1,8 +1,5 @@
 package today.candy.features.modules.impl.exploit;
 
-import net.minecraft.network.play.client.C0BPacketEntityAction;
-import net.minecraft.network.play.client.C0DPacketCloseWindow;
-import net.minecraft.network.play.client.C0EPacketClickWindow;
 import today.candy.event.annotations.EventTarget;
 import today.candy.event.impl.network.PacketSendEvent;
 import today.candy.features.modules.Category;
@@ -10,18 +7,7 @@ import today.candy.features.modules.Module;
 import today.candy.utils.network.PacketUtils;
 
 public class MoveDisabler extends Module {
-
     public MoveDisabler() {
         super("MoveDisabler",Category.EXPLOIT,"Move Disabler");
-    }
-
-    @EventTarget
-    public void onPacketSend(PacketSendEvent e) {
-        if ((e.getPacket() instanceof C0EPacketClickWindow || e.getPacket() instanceof C0DPacketCloseWindow) && mc.thePlayer.isSprinting()) {
-            e.setCancelled(true);
-            PacketUtils.sendPacket(new C0BPacketEntityAction(mc.thePlayer, C0BPacketEntityAction.Action.STOP_SPRINTING));
-            PacketUtils.sendPacketNoEvent(e.getPacket());
-            PacketUtils.sendPacket(new C0BPacketEntityAction(mc.thePlayer, C0BPacketEntityAction.Action.START_SPRINTING));
-        }
     }
 }
